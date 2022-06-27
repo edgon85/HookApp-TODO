c98b4ccaf88077ba20da92571a289c0dde940449
Edwin <edgon85@gmail.com>
2022-06-27 11:43:11 -0600

    hook useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HookApp } from './HookApp'
+import { CounterApp } from './01-useState/CouterApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HookApp />
+    <CounterApp />
   </React.StrictMode>
 )
