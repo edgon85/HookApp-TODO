a2b647ff7e940d61fb3642fa03f17456d03b104a
Edwin <edgon85@gmail.com>
2022-06-30 17:56:02 -0600

    prod 1.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-// import { TodoApp } from './08-useReducer/TodoApp'
+import { TodoApp } from './08-useReducer/TodoApp'
 import { MainApp } from './09-useContext/MainApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <TodoApp />
+  </React.StrictMode>
+/*   <React.StrictMode>
     <BrowserRouter>
       <MainApp />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode> */
 )
